9fa402039426db863a2941581ce630361f4c6b63
szymon-rogus <sszymek98@gmail.com>
2025-04-20 22:31:16 +0200

    counter2

--- easy/counter.js
+++ easy/counter.js
@@ -6,3 +6,14 @@ export var createCounter = function(n) {
         return n++;
     };
 };
+
+/// https://leetcode.com/problems/counter-ii
+export var createCounter2 = function(init) {
+    let currentCount = init;
+
+    return {
+        increment : () => { return ++currentCount},
+        decrement : () => { return --currentCount},
+        reset : () => { return currentCount = init}
+    }
+};
