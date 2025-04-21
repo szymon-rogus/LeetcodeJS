f612c2edd4ae823c3a84233d2849e06f672bfb86
szymon-rogus <sszymek98@gmail.com>
2025-04-21 17:48:14 +0200

    index methods import

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+import './easy/arrayPrototypeLast.js'
+import {createCounter, createCounter2} from "./easy/counter.js";
+import {filter} from "./easy/filterElementsfromArray.js";
+import {compose} from "./easy/functionComposition.js";
+import {sleep} from "./easy/sleep.js";
+import {sortBy} from "./easy/sortBy.js";
+import {cancellable} from "./easy/timeoutCancellation.js";
+import {expect} from "./easy/toBeOrNotToBe.js";
