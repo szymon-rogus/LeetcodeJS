76a016a953c84a24a12ccfe76bb3786f0d073e5e
szymon-rogus <sszymek98@gmail.com>
2025-04-23 18:29:02 +0200

    isObjectEmpty

---
+++ easy/json/isObjectEmpty.js
@@ -0,0 +1,6 @@
+/// https://leetcode.com/problems/is-object-empty
+
+export var isEmpty = function(obj) {
+    if (obj instanceof Array) return obj.length === 0;
+    if (obj instanceof Object) return JSON.stringify(obj) === '{}';
+};
