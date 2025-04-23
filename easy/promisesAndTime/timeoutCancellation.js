5327dc9085dd0581253d8fa7d8e2daf76d231154
szymon-rogus <sszymek98@gmail.com>
2025-04-23 18:29:25 +0200

    timeoutCancellation fix

--- easy/promisesAndTime/timeoutCancellation.js
+++ easy/promisesAndTime/timeoutCancellation.js
@@ -6,3 +6,13 @@ export var cancellable = function(fn, args, t) {
         clearTimeout(timer);
     }
 };
+
+/// https://leetcode.com/problems/interval-cancellation
+
+export var intervalCancellable = function(fn, args, t) {
+    fn(...args);
+    let timer = setInterval(() => fn(...args), t);
+    return function() {
+        clearTimeout(timer);
+    }
+};
