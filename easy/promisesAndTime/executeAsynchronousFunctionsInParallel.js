c6150a5f8da7576b6e01856925bdfa52b0986634
szymon-rogus <sszymek98@gmail.com>
2025-05-03 19:53:14 +0200

    executeAsynchronousFunctionsInParallel

---
+++ easy/promisesAndTime/executeAsynchronousFunctionsInParallel.js
@@ -0,0 +1,23 @@
+/// https://leetcode.com/problems/execute-asynchronous-functions-in-parallel
+
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+export var promiseAll = function(functions) {
+    let promises = functions.map(f => f());
+
+    return new Promise((resolve, reject) => {
+        let finished = [];
+        let finishedCount = 0;
+        for (let i = 0; i < promises.length; i++) {
+            promises[i].then(value => {
+                finished[i] = value;
+                finishedCount++;
+                if (finishedCount === promises.length) {
+                    resolve(finished);
+                }
+            }).catch(error => reject(error));
+        }
+    });
+};
